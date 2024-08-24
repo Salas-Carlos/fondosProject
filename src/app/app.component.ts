8600e4bd55cda518e2c4672de1d36d1afd3733cb
unknown <carlosan1999@hotmail.com>
2024-08-24 15:01:58 -0500

    implement import json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { get } from '@aws-amplify/api';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,30 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'fondoproject';
+
+  async fetchData() {
+    const apiName = 'tuApiNombre'; // Nombre del API configurado en Amplify
+    const path = '/ruta-de-tu-funcion'; // Ruta para acceder a la función Lambda
+
+    try {
+      const restOperation = get({
+        apiName: 'fondosAPI',
+        path: '/clients',
+
+      });
+      const { body } = await restOperation.response;
+      const response = await body.json();
+
+      console.log('POST call succeeded');
+      console.log(response);
+    } catch (error) {
+      console.error('Error al hacer la solicitud GET:', error);
+    }
+  }
+
+  ngOnInit() {
+
+
+    this.fetchData();
+  }
 }
